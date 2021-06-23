8cafb6fa8aececef58a6d4b4fa8a78e5e1f3c506
elboj <adaja.tobi1@gmail.com>
2021-06-23 15:58:45 +0100

    Updates

---
+++ precache-manifest.ff3923116e6d465541cf9d397cb0490f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf1b00632a1c08fdcf21f26052d0251",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "350468390bcc90fe1610",
+    "url": "/movie-search/static/css/main.013347e0.chunk.css"
+  },
+  {
+    "revision": "c858f52cbc7b21396e07",
+    "url": "/movie-search/static/js/2.59347f02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search/static/js/2.59347f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350468390bcc90fe1610",
+    "url": "/movie-search/static/js/main.5454eac8.chunk.js"
+  },
+  {
+    "revision": "ff9a2747beff396fe616",
+    "url": "/movie-search/static/js/runtime-main.05fddf4a.js"
+  }
+]);
